839359bf66238c6c361dea3f15785c6874703915
Pooja2773 <104152822+Pooja2773@users.noreply.github.com>
2022-04-30 15:02:45 +0530

    Add files via upload

---
+++ swapping program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+
+{
+    class Demo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            b = a + b;
+            Console.WriteLine("after the swapping number is:{0} ", a);
+            Console.WriteLine("after the swapping number is: {0}", b);
+        }
+    }
+}
